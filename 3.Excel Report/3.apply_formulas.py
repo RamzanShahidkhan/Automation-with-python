f6e929c19a2f00988d6c586a09dd04e48553b731
Ramzan Shahid <mramzan4422@gmail.com>
2022-08-07 11:54:11 +0500

    Add files via upload

--- 3.Excel Report/3.apply_formulas.py
+++ 3.Excel Report/3.apply_formulas.py
@@ -9,9 +9,11 @@ max_column = wb.active.max_column
 min_row = wb.active.min_row
 max_row = wb.active.max_row
 
+# Write an Excel formula with Python
 # sheet['B8'] = '=SUM(B6:B7)'
 # sheet['B8'].style = 'Currency'
 
+# Write multiple formulas with a for loop
 for i in range(min_column+1, max_column+1):
     # print(i)
     letter = get_column_letter(i)
